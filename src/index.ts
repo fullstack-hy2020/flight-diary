2ab486bd28cbb3adad28aa6f25cf24deef1c155f
rouanis5 <ouanis42@gmail.com>
2023-03-01 00:38:48 +0100

    fix missing cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
-const app = express();
+import cors from 'cors';
 import diaryRouter from './routes/diaries';
+
+const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3000;
